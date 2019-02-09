857825f0349538779f737c595a106d949b3dbbac
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-02-09 23:44:37 +0100

    handle basic enemy movement

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ class Enemy{
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y=y;
+    this.speed = 40;
   }
 
   // Update the enemy's position, required method for game
@@ -17,6 +18,11 @@ class Enemy{
       // You should multiply any movement by the dt parameter
       // which will ensure the game runs at the same speed for
       // all computers.
+      this.x+=this.speed*dt;
+      if(this.x>ctx.canvas.width){
+        this.x=0;
+      }
+
   }
 
   // Draw the enemy on the screen, required method for game
